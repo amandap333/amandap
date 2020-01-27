2d4841f4277beb742dffb3b57caceb32994abf5e
Amanda Proffit <Amandamarie2567@gmail.comm>
2020-01-27 09:21:23 -0700

    added bootstrap link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
+
+
+<head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</link>
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></meta>
+<title> Amanda Proffit Portfolio</title>
+</head> 
+
 import React from "react"
 import { Link } from "gatsby"
 import styles from "./about-css-modules.module.css"
@@ -6,18 +15,22 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+<body> 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <Link to="/contact/">  Contact</Link>
-    <h1>Hi people</h1>
+    <div style={{ margin: `3rem auto`, maxWidth: 600 }}></div>
+    <h1>Welcome to my portfolio!</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2  </Link>
+    
   </Layout>
 )
-
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body> 
 export default IndexPage
