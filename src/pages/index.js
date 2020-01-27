b5ed76fd19cca1bd99b1fd534126ecfbeefe6ec9
Amanda Proffit <Amandamarie2567@gmail.comm>
2020-01-27 09:30:36 -0700

    deleted link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,4 @@
 
-
-<head>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-</link>
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></meta>
-<title> Amanda Proffit Portfolio</title>
-</head> 
-
 import React from "react"
 import { Link } from "gatsby"
 import styles from "./about-css-modules.module.css"
@@ -15,7 +7,7 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-<body> 
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
@@ -30,5 +22,5 @@ const IndexPage = () => (
   </Layout>
 )
 
-</body> 
+
 export default IndexPage
