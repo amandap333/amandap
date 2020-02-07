3afa2ef2920898b2ab2ade279eddc4d04708909d
Amanda Proffit <Amandamarie2567@gmail.comm>
2020-02-07 11:46:50 -0700

    worked with form class/

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,19 +1,16 @@
 import React from "react"
 
-
 import Layout from "../components/layout"
 import SEO from '../components/seo'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
+
+import ContactForm from '../components/form/ContactForm'
+
 
 const ContactPage = ({ location }) => {
-  const handleFormSubmit = (e) => {
 
-  }
-  
   return (
     <Layout location={location}>
       <SEO 
@@ -24,23 +21,7 @@ const ContactPage = ({ location }) => {
       <Row>
           <Col>
             <h1>Contact me!</h1>
-            <Form>
-            <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text>
-              </Form.Group>
-
-              <Form.Group controlId="exampleForm.ControlTextarea1">
-                <Form.Label>Send me a message!</Form.Label>
-                <Form.Control as="textarea" rows="3" />
-              </Form.Group>
-              <Button variant="primary" type="submit" onClick={handleFormSubmit}>
-                Submit
-              </Button>
-            </Form>
+            <ContactForm />
           </Col>
         </Row>
       </Container>
