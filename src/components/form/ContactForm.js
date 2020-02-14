cc6732010ec40c0fcd2f35c788930145b3782fd7
Amanda Proffit <Amandamarie2567@gmail.comm>
2020-02-14 10:14:31 -0700

    added hidden/

--- src/components/form/ContactForm.js
+++ src/components/form/ContactForm.js
@@ -4,24 +4,27 @@ import React from 'react'
 
 const ContactForm = () => {
   return (
+
     <form name="contact" method="POST" data-netlify="true">
-    <p>
-      <label>Your First Name: <input type="text" name="firstname" /></label>   
-    </p>
-    <p>
-      <label>Your Last Name: <input type="text" name="lastname" /></label>   
-    </p>
-    <p>
-      <label>Your Email: <input type="email" name="email" /></label>
-    </p>
-    <p>
-      <label>Message: <textarea name="message"></textarea></label>
-    </p>
-    <p>
-      <button type="submit">Send</button>
-    </p>
-  </form>
-  )} 
+      <input type="hidden" name="form-name" value="contact" />
+        <p>
+          <label>Your First Name: <input type="text" name="firstname" /></label>  
+        </p>
+        <p>
+          <label>Your Last Name: <input type="text" name="lastname" /></label>   
+        </p>
+        <p>
+          <label>Your Email: <input type="email" name="email" /></label>
+        </p>
+        <p>
+          <label>Message: <textarea name="message"></textarea></label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+    </form>
+  )
+} 
 
   export default ContactForm
   
