3e15574a44dcc90fd66614c5c3e7a25886c222d8
Amanda Proffit <Amandamarie2567@gmail.comm>
2020-02-14 10:00:40 -0700

    added netfli from test site/

--- src/components/form/ContactForm.js
+++ src/components/form/ContactForm.js
@@ -6,16 +6,13 @@ const ContactForm = () => {
   return (
     <form name="contact" method="POST" data-netlify="true">
     <p>
-      <label>Your Name: <input type="text" name="name" /></label>   
+      <label>Your First Name: <input type="text" name="firstname" /></label>   
+    </p>
+    <p>
+      <label>Your Last Name: <input type="text" name="lastname" /></label>   
     </p>
     <p>
       <label>Your Email: <input type="email" name="email" /></label>
-    </p>
-    <p>
-      <label>Your Role: <select name="role[]" multiple>
-        <option value="leader">Leader</option>
-        <option value="follower">Follower</option>
-      </select></label>
     </p>
     <p>
       <label>Message: <textarea name="message"></textarea></label>
