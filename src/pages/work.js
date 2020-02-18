ff81ffae72c41f7e1fa50467db4b833709bdc968
Amanda Proffit <Amandamarie2567@gmail.comm>
2020-02-18 13:35:36 -0700

    changed file

--- src/pages/work.js
+++ src/pages/work.js
@@ -15,7 +15,7 @@ return (
     />
     <div style={{}}>
     <h1>Resume!</h1>
-    <iframe src="/Users/aproffit/Desktop/projects/amandap/amanda_resume.pdf" height="400"></iframe>
+    <iframe  title="myResume" src="/assets/resume.pdf." height="700"  width ="600" alt="resume"></iframe>
     </div>
   </Layout>
 )
