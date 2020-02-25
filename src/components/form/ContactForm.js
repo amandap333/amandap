be0722e2a185bf209861799008412577651ddcc5
Amanda Proffit <Amandamarie2567@gmail.comm>
2020-02-25 10:48:17 -0700

    changed Form Button to buttonl

--- src/components/form/ContactForm.js
+++ src/components/form/ContactForm.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import  { useForm } from 'react-hook-form'
-import Button from 'react-bootstrap/Button'
-import Form from 'react-bootstrap/Form'
+// import Button from 'react-bootstrap/Button'
+// import Form from 'react-bootstrap/Form'
 
 
 const ContactForm = () => {
